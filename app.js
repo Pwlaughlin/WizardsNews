66afd0173bedcca67b9b6d5e050495b684bcf609
Peter Laughlin <pwlaughlin@gmail.com>
2023-04-18 00:50:50 -0500

    done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const postBank = require("./postBank");
- 
+
 
 const app = express();
 
@@ -47,6 +47,26 @@ app.get('/', (req, res) =>{
 app.get('/posts/:id', (req, res) => {
   const id = req.params.id;
   const post = postBank.find(id);
+  if (!post.id) {
+    // If the post wasn't found, set the HTTP status to 404 and send Not Found HTML
+    res.status(404)
+    const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class="not-found">
+        <p>404: Page Not Found</p>
+        <img src ="https://media.tenor.com/UUS_Z-t_R-QAAAAM/wizard-face-palm.gif"/>
+      </div>
+    </body>
+    </html>`
+    res.send(html)
+  } else {
   res.send(`<!DOCTYPE html>
     <html>
     <head>
@@ -67,8 +87,9 @@ app.get('/posts/:id', (req, res) => {
       </div>
       </body>
     </html>`);
-});
-  
+  };
+  });
+
 
 const PORT = 1337;
 
